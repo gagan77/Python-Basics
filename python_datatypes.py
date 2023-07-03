a6ed596e69df9ea9998462d4c12fc8630c895b69
Gagan Cheema <gcheema7@hotmail.com>
2023-07-03 15:08:31 -0600

    Add files via upload

---
+++ python_datatypes.py
@@ -0,0 +1,12 @@
+a = 10.0;
+
+print(type(a))
+
+b = 2
+print(type(b))
+
+c= "asdasdfdgdfg"
+print(type(c))
+
+d = [1,2,3,4,5,6]
+print(type(d))
---
+++ python_input_output.py
@@ -0,0 +1,6 @@
+num1 = int(input("Please enter a first number: "))
+num2 = int(input("Please enter a second number: "))
+
+print(type(num1))
+
+print(num1 + num2)
