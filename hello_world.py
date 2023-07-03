3f385e6c7ad800ef73d6b6a8c6fcfae6eda7b5cf
gcheema77 <gcheema7@hotmail.com>
2023-07-03 15:26:19 -0600

    Basics of python updated

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,4 @@
+#A message every newbie should print out before doing anything else :)
 print("hello world")
+
+#this is a comment which is not executed by the interpreter
--- python_input_output.py
+++ python_input_output.py
@@ -1,6 +1,17 @@
+#Every application will reqiure data to come into it or be passed in by used.
+#This is where input comes in.
+#It is a function that allows you to take in data from the user.
+
+#The following code asks the user for two numbers and adds them together.
 num1 = int(input("Please enter a first number: "))
 num2 = int(input("Please enter a second number: "))
 
+#prints the type of the variable num1, if one is a float and one int then float will be the result
+#always use type to makesure you are getting the right type of data
 print(type(num1))
 
+#prints the summation of the two numbers
 print(num1 + num2)
+
+# Check out more about this on
+# https://www.w3schools.com/python/ref_func_type.asp
