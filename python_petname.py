46ae64cb22328f45084a9609382bfb84a5b497f4
gcheema77 <gcheema7@hotmail.com>
2023-07-06 12:09:08 -0600

    demo of random function

---
+++ python_petname.py
@@ -0,0 +1,7 @@
+import random
+
+f = open("petnames.txt", "r") #open file
+f_content = f.read() #read file content
+f_content_list = f_content.split("\n") #split file content into list
+print("The random chosen pet name is:")
+print(random.choice(f_content_list))
