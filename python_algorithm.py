e7b2e1bc682225813f74d52aac91e5ca3a886bd6
gcheema77 <gcheema7@hotmail.com>
2023-07-06 16:41:57 -0600

    Palindrome algo

---
+++ python_algorithm.py
@@ -0,0 +1,13 @@
+#Algortihm for a palindrome
+# Palindrome is a word, phrase, or sequence that reads the same backward as forward.
+
+def isPalindrome(str):
+    startIndex = 0
+    endIndex = len(str) - 1
+
+    for x in str:
+        if str[startIndex] != str[endIndex]:
+            return False
+    return True
+
+print(isPalindrome("racecars"))
