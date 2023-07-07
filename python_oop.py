23c116da24c5268940d469dfc8d89e9713f938a2
gcheema77 <gcheema7@hotmail.com>
2023-07-07 11:57:12 -0600

    oop principles

---
+++ python_oop.py
@@ -0,0 +1,9 @@
+# Object Oriented Programming (1 of many Paradigms)
+# relies on simplicity and flexibility to reduce workflow
+# High modularity which makes it easier to maintain and extend the code
+
+# Classes
+# Classes are blueprints for objects (contains Attributes and Behaviors)
+
+# Encapsulation
+# The idea of encapsulation is to have methods and variables within the bounds of a given unit.
