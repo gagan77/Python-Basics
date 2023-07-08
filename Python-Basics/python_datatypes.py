09d5d974b1e6055ee95a05d8c6e639d8549ce028
gcheema77 <gcheema7@hotmail.com>
2023-07-08 17:00:08 -0600

    Fixed file structure

---
+++ Python-Basics/python_datatypes.py
@@ -0,0 +1,23 @@
+#Python is a dynamically typed language
+#It consists of 8 types of data types
+#This shows the Numeric Type & Sequence type
+#some of these are shown below
+
+#float
+a = 10.0;
+print(type(a))
+
+#integer
+b = 2
+print(type(b))
+
+#string
+c= "asdasdfdgdfg"
+print(type(c))
+
+#list
+d = [1,2,3,4,5,6]
+print(type(d))
+
+#Checkout more about this on 
+# https://www.w3schools.com/python/python_datatypes.asp
---
+++ Python-Basics/python_variables.py
@@ -0,0 +1,16 @@
+#a represents a variable 
+a = 10;
+print(a)
+
+# the statement below deletes the variable a
+del a;
+print(a)
+
+# Just basics of variables in python
+# you can declare multiple variables in one line
+a = b = c = d = 10;
+
+# you can also assign multiple values to multiple variables in one line
+a,b,c,d = 10,20,30,40;
+
+# Checkout more on variables in python here: https://www.geeksforgeeks.org/python-variables-navercorp.com/
